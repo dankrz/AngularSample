9ae24e441efc64e1ca0b3c7b0c8aa042783a2b3e
Krzysztof Danielewicz <krzysztof.danielewicz@volvo.com>
2018-09-10 10:19:21 +0200

    Test for the smart component UsersPageComponent.

--- src/app/features/users/containers/users-page/users-page.component.spec.ts
+++ src/app/features/users/containers/users-page/users-page.component.spec.ts
@@ -1,24 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UsersPageComponent } from './users-page.component';
-import {combineReducers, StoreModule} from '@ngrx/store';
-import {usersPageReducer} from './users-page.reducer';
+import {UsersPageComponent} from './users-page.component';
+import {Store} from '@ngrx/store';
+import {MockStore} from '../../../../../tests/mocked-services';
+import {State} from '../../users.module.reducers';
+import {AddUser, ChangeSearchQuery, EditUser, LoadUsers, RemoveUser} from './users-page.actions';
+import {User} from '../../models/user';
 
 describe('UsersPageComponent', () => {
   let component: UsersPageComponent;
-  let fixture: ComponentFixture<UsersPageComponent>;
+  let fixture: ComponentFixture<UsersPageComponent>,
+    store: MockStore<State>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        StoreModule.forRoot({
-          usersPage: combineReducers(usersPageReducer),
-        }),
+      providers: [
+        {provide: Store, useClass: MockStore}
       ],
-      declarations: [ UsersPageComponent ]
+      declarations: [UsersPageComponent]
     })
-    .overrideTemplate(UsersPageComponent, '')
-    .compileComponents();
+      .overrideTemplate(UsersPageComponent, '')
+      .compileComponents();
+    store = TestBed.get(Store);
   }));
 
   beforeEach(() => {
@@ -30,4 +33,63 @@ describe('UsersPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load users', () => {
+    expect(store.dispatch).toHaveBeenCalledWith(new LoadUsers());
+  });
+
+
+  it('should dispatch AddUser onAddUser event', () => {
+    const user: User = {
+      id: 0,
+      surname: 'sn',
+      phone: '123',
+      city: 'Some',
+      street: 'Another',
+      number: 2,
+      birthDate: new Date(2001, 10, 4),
+      name: 'test'
+    };
+    component.onAddUser(user);
+    expect(store.dispatch).toHaveBeenCalledWith(new AddUser(user));
+  });
+
+
+  it('should dispatch EditUser onEditUser event', () => {
+    const user: User = {
+      id: 0,
+      surname: 'sn',
+      phone: '123',
+      city: 'Some',
+      street: 'Another',
+      number: 2,
+      birthDate: new Date(2001, 10, 4),
+      name: 'test'
+    };
+    component.onEditUser(user);
+    expect(store.dispatch).toHaveBeenCalledWith(new EditUser(user));
+  });
+
+  it('should dispatch RemoveUser onRemoveUser event', () => {
+    const user: User = {
+      id: 0,
+      surname: 'sn',
+      phone: '123',
+      city: 'Some',
+      street: 'Another',
+      number: 2,
+      birthDate: new Date(2001, 10, 4),
+      name: 'test'
+    };
+    component.onRemoveUser(user);
+    expect(store.dispatch).toHaveBeenCalledWith(new RemoveUser(user));
+  });
+
+  it('should dispatch ChangeSearchQuery onSearchQueryChange event', () => {
+    const searchQuery = 'test';
+    component.onSearchQueryChange(searchQuery);
+    expect(store.dispatch).toHaveBeenCalledWith(new ChangeSearchQuery(searchQuery));
+  });
+
+
 });
--- src/tests/mocked-services.ts
+++ src/tests/mocked-services.ts
@@ -1,4 +1,17 @@
+import {BehaviorSubject} from 'rxjs';
+import {Store} from '@ngrx/store';
+
 export function mockService(serviceClass: any): any {
   const props = Object.getOwnPropertyNames(serviceClass.prototype).slice(1);
   return jasmine.createSpyObj(props);
+}
+
+export class MockStore<T> {
+  public pipe: jasmine.Spy = jasmine.createSpy('pipe');
+  public dispatch: jasmine.Spy;
+
+  constructor() {
+    Object.assign(this, mockService(Store));
+    this.pipe.and.returnValue(new BehaviorSubject<any>({}));
+  }
 }
